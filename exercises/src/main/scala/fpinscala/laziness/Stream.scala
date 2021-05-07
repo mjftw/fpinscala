ac68230c72d437eef7d19aeea740ffba1f444f22
Merlin Webster <mjftwebster@gmail.com>
2021-05-07 15:50:17 +0100

    Copy answers for Stream ex7 because lazy

--- exercises/src/main/scala/fpinscala/laziness/Stream.scala
+++ exercises/src/main/scala/fpinscala/laziness/Stream.scala
@@ -53,6 +53,21 @@ trait Stream[+A] {
   // 5.7 map, filter, append, flatmap using foldRight. Part of the exercise is
   // writing your own function signatures.
 
+  def map[B](f: A => B): Stream[B] =
+    foldRight(empty[B])((h,t) => cons(f(h), t)) 
+
+  def filter(f: A => Boolean): Stream[A] =
+    foldRight(empty[A])((h,t) => 
+      if (f(h)) cons(h, t)
+      else t) 
+
+  def append[B>:A](s: => Stream[B]): Stream[B] = 
+    foldRight(s)((h,t) => cons(h,t))
+
+  def flatMap[B](f: A => Stream[B]): Stream[B] = 
+    foldRight(empty[B])((h,t) => f(h) append t)
+
+
   def startsWith[B](s: Stream[B]): Boolean = ???
 }
 case object Empty extends Stream[Nothing]
